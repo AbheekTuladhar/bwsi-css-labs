d54c1c0938557bce321c6b563699368c83e5e5a7
JediCubing <abheektuladhar@gmail.com>
2026-03-15 01:22:30 -0400

    Update lab_1a.py
    New Print statement for practice

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,10 @@ def main():
     name = "Abheek Tuladhar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Abheek is a programmer for FRC Team 3260, the founder of the Pine-Richland Gavel Club, and a Top 10 finalist in the international LingHacks VI hackathon. " \
+    "As the chapter head of Pine-Richland Steel City Codes, he mentors middle-school future coders. Abheek also plays the piano and violin, through which he has earned " \
+    "numerous state and regional awards. Additionally, in his free time, he is a practitioner of the discipline Muay Thai and an avid chess player. Abheek plans to pursue " \
+    "artificial intelligence and computer science after high school.")
 
 if __name__ == "__main__":
     main()
